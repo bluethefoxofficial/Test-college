81c7f403f14c42301db564cecb7043cc67f56a29
Kessan Robertson (Bluethefox) <47904806+bluethefoxofficial@users.noreply.github.com>
2022-09-08 15:00:53 +0100

    e
    3

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,37 @@
 
 #include <iostream>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
 using std::string;
 int main() {
 	string x;
-	cout << strnl("what if your name") << "name>";
+	cout << "what if your name\n" << "name>";
 	//input name from user.
 	cin >> x;
 	//output the name with hello so "hello example name"
 	cout << "hello " << x << "\n";
 
-	//speak out the value of x.
+	//M O R S E
 
+	char text[36] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1','2','3','4','5','6','7','8', '9','0' };
+
+	string morse[36] = { ".-","-...","-.-.","-..",".","..-","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
+	".----","..---","...--","....-",".....","-....","--....","---..","----.","-----" };
+
+	for (int i = 0; i < x.length(); i++) {
+
+		if (phrase.at(i) == text[counter])
+		{
+			std::cout << morse[counter];
+			
+		}
+
+	}
 
 
 	return 0;
 }
 
 
-string strnl(string out) {
-	
-	string nl = out + "\n";
-
-
-	return nl;
-}
